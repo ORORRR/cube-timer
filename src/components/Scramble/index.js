ddaa39d086d3d5bc577612b1dbd7aa43d54b57ce
ORORRR <v.aurore.av@gmail.com>
2020-08-03 16:15:31 +0200

    created scramble et timer components

---
+++ src/components/Scramble/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+
+const Scramble = (props)  => {
+    return <h1>R D L D2 R </h1>;
+}
+
+export default Scramble;
---
+++ src/components/Timer/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+
+const Timer = (props)  => {
+    return <h1>Timer</h1>;
+}
+
+export default Timer;
