9a9f86e82ac0eb65a4abdd934d28cfaaa792e60e
ORORRR <v.aurore.av@gmail.com>
2020-08-04 23:46:48 +0200

    timer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Scramble from './components/Scramble'
 import Timer from './components/Timer'
 
-function App() {
+const App = () => {
   return (
     <div className="App">
+      <header className="App-header">
+        <h1>Cube Timer</h1>
+      </header>
+
       <Scramble></Scramble>
       <Timer></Timer>
     </div>
--- src/components/Timer/index.js
+++ src/components/Timer/index.js
@@ -1,8 +1,120 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react'
+const prettyMilliseconds = require('pretty-ms')
 
 
-const Timer = (props)  => {
-    return <h1>Timer</h1>;
+const Timer = ()  => {
+    const [time, setTime] = useState(0)
+    const [startTime, setStartTime] = useState(0)
+    const [isOn, setIsOn] = useState(false)
+
+    useEffect(() => {
+        let interval = null;
+        if (isOn) {
+          interval = setInterval(() => {
+            setTime(time => Date.now() - startTime);
+          }, 1000)
+        } else if (!isOn && time !== 0) {
+          clearInterval(interval)
+        }
+
+        return () => clearInterval(interval)
+    //   }, [isOn, time]);
+    }, [isOn]);
+
+
+
+    const startTimer = () => {
+        setTime(0)
+        setStartTime(Date.now())
+        setIsOn(true)
+    }
+
+    const stopTimer = () => {
+        setIsOn(false)
+    }
+      
+    const resetTimer = () => {
+        stopTimer()
+        setTime(0)
+    }
+
+    let startButton = (time === 0 && !isOn) ?
+        <button onClick={startTimer}>start</button> :
+        null
+    let stopButton = (isOn) ?
+        <button onClick={stopTimer}>stop</button> :
+        null
+    let resetButton = (time !== 0 && !isOn) ?
+        <button onClick={resetTimer}>reset</button> :
+        null
+
+    return (
+        <>
+            <h1>Timer</h1>
+            <h3>{prettyMilliseconds(time, {colonNotation: true, secondsDecimalDigits: 2, keepDecimalsOnWholeSeconds: true})}</h3>
+
+            {startButton}
+            {stopButton}
+            {resetButton}
+        </>
+    );
+
+
 }
+
+
+// class Timer extends React.Component {
+//     constructor(props){
+//       super(props)
+//       this.state = {
+//         time: 0,
+//         start: 0,
+//         isOn: false
+//       }
+//       this.startTimer = this.startTimer.bind(this)
+//       this.stopTimer = this.stopTimer.bind(this)
+//       this.resetTimer = this.resetTimer.bind(this)
+//     }
+//     startTimer() {
+//       this.setState({
+//         time: this.state.time,
+//         start: Date.now() - this.state.time,
+//         isOn: true
+//       })
+//       this.timer = setInterval(() => this.setState({
+//         time: Date.now() - this.state.start
+//       }), 1);
+//     }
+//     stopTimer() {
+//       this.setState({isOn: false})
+//       clearInterval(this.timer)
+//     }
+//     resetTimer() {
+//       this.setState({time: 0})
+//     }
+//     render() {
+//       let start = (this.state.time == 0) ?
+//         <button onClick={this.startTimer}>start</button> :
+//         null
+//       let stop = (this.state.isOn) ?
+//         <button onClick={this.stopTimer}>stop</button> :
+//         null
+//       let reset = (this.state.time != 0 && !this.state.isOn) ?
+//         <button onClick={this.resetTimer}>reset</button> :
+//         null
+//       let resume = (this.state.time != 0 && !this.state.isOn) ?
+//         <button onClick={this.startTimer}>resume</button> :
+//         null
+//       return(
+//         <div>
+//           <h3>timer: {this.state.time}</h3>
+//           {start}
+//           {resume}
+//           {stop}
+//           {reset}
+//         </div>
+//       )
+//     }
+//   }
 
 export default Timer;
