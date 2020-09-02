00891a6712c84811daa15954334bcc27d9990581
ORORRR <v.aurore.av@gmail.com>
2020-09-02 22:59:17 +0200

    Updates

---
+++ precache-manifest.f603dbdd67329ea9a6f82e4b7257448d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57840ab3829e06980dac60d745b88254",
+    "url": "/cube-timer/index.html"
+  },
+  {
+    "revision": "5ef24db49fb3295af25e",
+    "url": "/cube-timer/static/css/main.9b941ead.chunk.css"
+  },
+  {
+    "revision": "a93612e27537c8057d3c",
+    "url": "/cube-timer/static/js/2.bfdd2f48.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/cube-timer/static/js/2.bfdd2f48.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ef24db49fb3295af25e",
+    "url": "/cube-timer/static/js/main.d26f3f7e.chunk.js"
+  },
+  {
+    "revision": "eff9aa9be2bacb418080",
+    "url": "/cube-timer/static/js/runtime-main.94463fe7.js"
+  }
+]);
